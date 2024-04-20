99866d91df502d281ce7575d8e62230d001dea1b
ionutmec <156110073+ionutmec@users.noreply.github.com>
2024-04-20 03:54:46 +0100

    my repository

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+
+<script>
+alert("Hello, World! I am an alert box");
+
+</script>
+
---
+++ Basic_HTML_and_JavaScript_1/Hello_World.js
@@ -0,0 +1,26 @@
+
+
+
+<!DOCTYPE html>
+<html lang = "en">
+
+<script>
+alert("Hello, World! I am an alert box");
+
+</script>
+
+
+<head>
+  <title>Javascript</title>
+  <meta charset = "utf-8">
+</head>
+
+<body>
+
+
+<h1> This is a text alert</h1>
+
+
+</body>
+
+</html>
