f1ccd9f381a6660e2b941566aceea603d134d073
ionutmec <156110073+ionutmec@users.noreply.github.com>
2024-04-27 17:45:48 +0100

    Update main.js

--- JavaScript/Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ JavaScript/Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -1,4 +1,8 @@
+var anA = "Ana are mere" + " si pere <br>";
+document.write(anA);
 
-document.write('Hello, World!,');
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro<br>";
+document.write(Dad);
+
 
 
